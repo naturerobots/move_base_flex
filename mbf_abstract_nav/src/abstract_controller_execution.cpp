decdb35fe6d4c13ac28867e097eb30023435d635
mrvsht <marvin.schulte96@googlemail.com>
2023-11-09 15:07:56 +0100

    add param descriptor

--- mbf_abstract_nav/src/abstract_controller_execution.cpp
+++ mbf_abstract_nav/src/abstract_controller_execution.cpp
@@ -43,6 +43,7 @@
 #include "mbf_abstract_nav/abstract_controller_execution.h"
 
 #include <tf2/utils.h>
+#include <parameter_value.hpp>
 
 namespace mbf_abstract_nav
 {
@@ -68,22 +69,26 @@ AbstractControllerExecution::AbstractControllerExecution(
 {
 
   // reconfigurable parameters
-  node_handle_->declare_parameter("robot_frame", rclcpp::ParameterValue(std::string("base_link")));
-  node_handle_->declare_parameter("map_frame", rclcpp::ParameterValue(std::string("map")));
-  node_handle_->declare_parameter("force_stop_at_goal", rclcpp::ParameterValue(false));
-  node_handle_->declare_parameter("force_stop_on_retry", rclcpp::ParameterValue(true));
-  node_handle_->declare_parameter("force_stop_on_cancel", rclcpp::ParameterValue(false));
-  node_handle_->declare_parameter("mbf_tolerance_check", rclcpp::ParameterValue(false));
-  node_handle_->declare_parameter("dist_tolerance", rclcpp::ParameterValue(0.1));
-  node_handle_->declare_parameter("angle_tolerance", rclcpp::ParameterValue(M_PI / 18.0));
-  node_handle_->declare_parameter("tf_timeout", rclcpp::ParameterValue(1.0));
-  node_handle_->declare_parameter("cmd_vel_ignored_tolerance", rclcpp::ParameterValue(5.0));
+  node_handle_->declare_parameter("robot_frame", "base_link");
+  node_handle_->declare_parameter("map_frame", "map");
+  node_handle_->declare_parameter("force_stop_at_goal", false);
+  node_handle_->declare_parameter("force_stop_on_retry", true);
+  node_handle_->declare_parameter("force_stop_on_cancel",false);
+  node_handle_->declare_parameter("mbf_tolerance_check", false);
+  node_handle_->declare_parameter("dist_tolerance", 0.1));
+  node_handle_->declare_parameter("angle_tolerance", M_PI / 18.0);
+  node_handle_->declare_parameter("tf_timeout", 1.0);
+  node_handle_->declare_parameter("cmd_vel_ignored_tolerance", 5.0);
 
-  node_handle->declare_parameter("controller_frequency", rclcpp::ParameterValue(20));
-  node_handle->declare_parameter("controller_patience", rclcpp::ParameterValue(5.0));
-  node_handle->declare_parameter("controller_max_retries", rclcpp::ParameterValue(-1));
+  auto param_desc = rcl_interfaces::msg::ParameterDescriptor{};
+  param_desc.description = "The rate in Hz at which to run the control loop and send velocity commands to the base";
+  node_handle->declare_parameter("controller_frequency", rclcpp::ParameterValue(20), param_desc);
+  param_desc.description = "How long the controller will wait in seconds without receiving a valid control before "
+                           "giving up";
+  node_handle->declare_parameter("controller_patience", rclcpp::ParameterValue(5.0), param_desc);
+  param_desc.description ="How many times we will recall the controller in an attempt to find a valid command before giving up";
+      node_handle->declare_parameter("controller_max_retries", rclcpp::ParameterValue(-1), param_desc);
 
-  
   node_handle_->get_parameter("robot_frame", robot_frame_);
   node_handle_->get_parameter("map_frame", global_frame_);
   node_handle_->get_parameter("force_stop_at_goal", force_stop_at_goal_);
