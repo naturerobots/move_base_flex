84bdac9201844244188bd77a577df4232f4f697e
Matthias Holoch <mholoch@gmail.com>
2023-12-01 13:33:18 +0100

    fix abstract action base test

--- mbf_abstract_nav/test/abstract_action_base.cpp
+++ mbf_abstract_nav/test/abstract_action_base.cpp
@@ -2,7 +2,7 @@
 #include <gtest/gtest.h>
 
 // dummy message
-#include <mbf_msgs/GetPathAction.h>
+#include <mbf_msgs/action/get_path.hpp>
 #include <mbf_utility/robot_information.h>
 
 #include <mbf_abstract_nav/abstract_action_base.hpp>
@@ -14,7 +14,7 @@ using namespace mbf_abstract_nav;
 struct MockedExecution : public AbstractExecutionBase {
   typedef boost::shared_ptr<MockedExecution> Ptr;
 
-  MockedExecution(const mbf_utility::RobotInformation& ri) : AbstractExecutionBase("mocked_execution", ri) {}
+  MockedExecution(const mbf_utility::RobotInformation& ri, const rclcpp::Node::SharedPtr& node) : AbstractExecutionBase("mocked_execution", ri, node) {}
 
   MOCK_METHOD0(cancel, bool());
 
@@ -26,17 +26,20 @@ using testing::Test;
 
 // fixture with access to the AbstractActionBase's internals
 struct AbstractActionBaseFixture
-    : public AbstractActionBase<mbf_msgs::GetPathAction, MockedExecution>,
+    : public AbstractActionBase<mbf_msgs::action::GetPath, MockedExecution>,
       public Test {
   // required members for the c'tor
-  TF tf_;
   std::string test_name;
+  rclcpp::Node::SharedPtr node_;
+  TF tf_;
   mbf_utility::RobotInformation ri;
 
   AbstractActionBaseFixture()
       : test_name("action_base"),
-        ri(tf_, "global_frame", "local_frame", ros::Duration()),
-        AbstractActionBase(test_name, ri)
+        node_(std::make_shared<rclcpp::Node>("test_node")),
+        tf_(node_->get_clock()),
+        ri(node_, tf_, "global_frame", "local_frame", rclcpp::Duration(0,0)),
+        AbstractActionBase(node_, test_name, ri)
   {
   }
 
@@ -46,7 +49,7 @@ struct AbstractActionBaseFixture
 TEST_F(AbstractActionBaseFixture, thread_stop)
 {
   unsigned char slot = 1;
-  concurrency_slots_[slot].execution.reset(new MockedExecution(AbstractActionBaseFixture::ri));
+  concurrency_slots_[slot].execution.reset(new MockedExecution(AbstractActionBaseFixture::ri, node_));
   concurrency_slots_[slot].thread_ptr =
       threads_.create_thread(boost::bind(&AbstractActionBaseFixture::run, this,
                                          boost::ref(concurrency_slots_[slot])));
@@ -58,7 +61,7 @@ TEST_F(AbstractActionBaseFixture, cancelAll)
 {
   // spawn a bunch of threads
   for (unsigned char slot = 0; slot != 10; ++slot) {
-    concurrency_slots_[slot].execution.reset(new MockedExecution(AbstractActionBaseFixture::ri));
+    concurrency_slots_[slot].execution.reset(new MockedExecution(AbstractActionBaseFixture::ri, node_));
     // set the expectation
     EXPECT_CALL(*concurrency_slots_[slot].execution, cancel())
         .WillRepeatedly(Return(true));
@@ -81,9 +84,7 @@ TEST_F(AbstractActionBaseFixture, cancelAll)
 
 int main(int argc, char **argv)
 {
-  // we need this only for kinetic and lunar distros
-  ros::init(argc, argv, "abstract_action_base");
-  ros::NodeHandle nh;
+  rclcpp::init(argc, argv);
   testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
 }
