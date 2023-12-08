a9a8860531529cb2a9a06885faea0c9f0576f060
Matthias Holoch <mholoch@gmail.com>
2023-12-08 17:20:46 +0100

    finalize migration of planner_action tests

--- mbf_abstract_nav/test/planner_action.cpp
+++ mbf_abstract_nav/test/planner_action.cpp
@@ -34,6 +34,7 @@ struct MockPlanner : public AbstractPlanner
 
 using mbf_abstract_nav::AbstractPlannerExecution;
 using mbf_msgs::action::GetPath;
+using ServerGoalHandlePtr = std::shared_ptr<rclcpp_action::ServerGoalHandle<GetPath>>;
 
 using testing::_;
 using testing::DoAll;
@@ -45,25 +46,6 @@ using testing::Test;
 using testing::MatcherCast;
 using testing::Pointee;
 using testing::SafeMatcherCast;
-
-// a mocked action server
-struct MockedActionServer : public rclcpp_action::Server<GetPath>
-{
-  // define the types we are using
-  typedef std::shared_ptr<rclcpp_action::ServerGoalHandle<GetPath>> GoalHandlePtr;
-
-  MockedActionServer(const rclcpp::Node::SharedPtr& node, GoalCallback goal_cb, CancelCallback cancel_cb, AcceptedCallback call_action_cb)
-    : rclcpp_action::Server<GetPath>(
-        node->get_node_base_interface(),
-        node->get_node_clock_interface(),
-        node->get_node_logging_interface(),
-        "mocked_test_server", rcl_action_server_get_default_options(),
-        goal_cb, cancel_cb, call_action_cb)
-  { }
-
-  // the mocked method
-  MOCK_METHOD2(publish_result, void(const rclcpp_action::GoalUUID &, std::shared_ptr<void>));
-};
 
 // test-fixture
 struct PlannerActionFixture : public Test
@@ -93,23 +75,18 @@ struct PlannerActionFixture : public Test
     ASSERT_TRUE(action_client_->wait_for_action_server(std::chrono::milliseconds(100)));
   }
 
-  void TearDown()
-  {
-    // wait until the test finishes, i.e. until the action_client gets a result returned from the action_server
-  }
-
   rclcpp_action::GoalResponse acceptGoal(const rclcpp_action::GoalUUID& uuid, mbf_msgs::action::GetPath::Goal::ConstSharedPtr goal) 
   {
    return rclcpp_action::GoalResponse::ACCEPT_AND_EXECUTE;
   }
 
-  rclcpp_action::CancelResponse cancelAction(MockedActionServer::GoalHandlePtr goal_handle)
+  rclcpp_action::CancelResponse cancelAction(ServerGoalHandlePtr goal_handle)
   {
     planner_action_.cancel(goal_handle);
     return rclcpp_action::CancelResponse::ACCEPT;
   }
 
-  void callAction(MockedActionServer::GoalHandlePtr goal_handle)
+  void callAction(ServerGoalHandlePtr goal_handle)
   {
     planner_action_.start(goal_handle, planner_execution_);
   }
@@ -135,7 +112,7 @@ struct PlannerActionFixture : public Test
 
   rclcpp::Node::SharedPtr node_;
   TFPtr tf_;
-  std::shared_ptr<MockPlanner> planner_;    ///< the mocked planner
+  std::shared_ptr<MockPlanner> planner_; ///< the mocked planner
   std::string global_frame_;
   std::string local_frame_;
   mbf_utility::RobotInformation robot_info_;
@@ -158,111 +135,119 @@ TEST_F(PlannerActionFixture, emptyPath)
   rclcpp_action::Client<mbf_msgs::action::GetPath>::WrappedResult result; // to be filled by sendGoalAndWaitForResult
   sendGoalAndWaitForResult(goal, result);
 
-  ASSERT_EQ(result.code, rclcpp_action::ResultCode::ABORTED);
+  EXPECT_EQ(result.code, rclcpp_action::ResultCode::ABORTED);
   EXPECT_EQ(result.result->outcome, mbf_msgs::action::GetPath::Result::EMPTY_PATH);
 }
 
-//TEST_F(PlannerActionFixture, success)
-//{
-//  // create a dummy path
-//  std::vector<geometry_msgs::msg::PoseStamped> path(10);
-//  // set the frame such that we can skip tf
-//  for (size_t ii = 0; ii != path.size(); ++ii)
-//  {
-//    path[ii].header.frame_id = global_frame_;
-//    path[ii].pose.orientation.w = 1;
-//  }
-//
-//  // goal with frames, so we can pass tf-lookup
-//  goal_->use_start_pose = true;
-//  goal_->start_pose.header.frame_id = goal_->target_pose.header.frame_id = global_frame_;
-//
-//  // setup the expectation
-//  EXPECT_CALL(*planner_, makePlan(_, _, _, _, _, _)).WillOnce(DoAll(SetArgReferee<3>(path), Return(0)));
-//  EXPECT_CALL(action_server_,
-//              publish_result(_, Field(&mbf_msgs::action::GetPath::Result::outcome, Eq(mbf_msgs::action::GetPath::Result::SUCCESS))))
-//      .Times(1)
-//      .WillOnce(Notify(&done_condition_));
-//}
-//
-//TEST_F(PlannerActionFixture, tfError)
-//{
-//  // create a dummy path
-//  std::vector<geometry_msgs::msg::PoseStamped> path(10);
-//  // set the frame such that we fail at the tf
-//  for (size_t ii = 0; ii != path.size(); ++ii)
-//    path[ii].header.frame_id = "unknown";
-//
-//  // setup the expectation - we succeed here
-//  EXPECT_CALL(*planner_, makePlan(_, _, _, _, _, _)).WillOnce(DoAll(SetArgReferee<3>(path), Return(0)));
-//  EXPECT_CALL(action_server_,
-//              publish_result(_, Field(&mbf_msgs::action::GetPath::Result::outcome, Eq(mbf_msgs::action::GetPath::Result::TF_ERROR))))
-//      .Times(1)
-//      .WillOnce(Notify(&done_condition_));
-//
-//  // goal with frames, so we can pass tf-lookup
-//  goal_->use_start_pose = true;
-//  goal_->start_pose.header.frame_id = goal_->target_pose.header.frame_id = global_frame_;
-//}
-//
-//TEST_F(PlannerActionFixture, noRobotPose)
-//{
-//  // test case where we fail to get a valid robot pose.
-//  // in this case we will receive a TF_ERROR from the server.
-//
-//  // make us use the robot pose
-//  goal_->use_start_pose = false;
-//  goal_->start_pose.header.frame_id = goal_->target_pose.header.frame_id = global_frame_;
-//
-//  // set the expectation
-//  EXPECT_CALL(action_server_,
-//              publish_result(_, Field(&mbf_msgs::action::GetPath::Result::outcome, Eq(mbf_msgs::action::GetPath::Result::TF_ERROR))))
-//      .Times(1)
-//      .WillOnce(Notify(&done_condition_));
-//}
-//
-//TEST_F(PlannerActionFixture, noPathFound)
-//{
-//  // test case where the planner fails.
-//  // in this case we will receive NO_PLAN_FOUND from the server.
-//  node_->set_parameter(rclcpp::Parameter("planner_max_retries", 0));
-//
-//  // valid goal
-//  goal_->use_start_pose = true;
-//  goal_->start_pose.header.frame_id = goal_->target_pose.header.frame_id = global_frame_;
-//
-//  // set the expectation: the planner returns a failure
-//  EXPECT_CALL(*planner_, makePlan(_, _, _, _, _, _)).WillOnce(Return(mbf_msgs::action::GetPath::Result::NO_PATH_FOUND));
-//  EXPECT_CALL(action_server_,
-//              publish_result(_, Field(&mbf_msgs::action::GetPath::Result::outcome, Eq(mbf_msgs::action::GetPath::Result::NO_PATH_FOUND))))
-//      .Times(1)
-//      .WillOnce(Notify(&done_condition_));
-//}
-//
-//ACTION(SleepAndFail)
-//{
-//  std::this_thread::sleep_for(std::chrono::milliseconds(10));
-//  return 11;
-//}
-//
-//TEST_F(PlannerActionFixture, patExceeded)
-//{
-//  // test case where the planner fails multiple times and we are out of patience
-//
-//  node_->set_parameter(rclcpp::Parameter("planner_max_retries", 5));
-//  node_->set_parameter(rclcpp::Parameter("planner_patience", 0.001));
-//
-//  // valid goal
-//  goal_->use_start_pose = true;
-//  goal_->start_pose.header.frame_id = goal_->target_pose.header.frame_id = global_frame_;
-//
-//  // set the expectation: the planner returns a failure
-//  EXPECT_CALL(*planner_, makePlan(_, _, _, _, _, _)).WillRepeatedly(SleepAndFail());
-//  EXPECT_CALL(action_server_,
-//              publish_result(_, Field(&mbf_msgs::action::GetPath::Result::outcome, Eq(mbf_msgs::action::GetPath::Result::PAT_EXCEEDED))))
-//      .Times(1)
-//      .WillOnce(Notify(&done_condition_));
-//}
+TEST_F(PlannerActionFixture, success)
+{
+  // create a dummy path
+  std::vector<geometry_msgs::msg::PoseStamped> path(10);
+  // set the frame such that we can skip tf
+  for (size_t ii = 0; ii != path.size(); ++ii)
+  {
+    path[ii].header.frame_id = global_frame_;
+    path[ii].pose.orientation.w = 1;
+  }
+
+  // goal with frames, so we can pass tf-lookup
+  mbf_msgs::action::GetPath::Goal goal;
+  goal.use_start_pose = true;
+  goal.start_pose.header.frame_id = goal.target_pose.header.frame_id = global_frame_;
+
+  // setup the expectation
+  EXPECT_CALL(*planner_, makePlan(_, _, _, _, _, _)).WillOnce(DoAll(SetArgReferee<3>(path), Return(0)));
+
+  rclcpp_action::Client<mbf_msgs::action::GetPath>::WrappedResult result;
+  sendGoalAndWaitForResult(goal, result);
+  EXPECT_EQ(result.code, rclcpp_action::ResultCode::SUCCEEDED);
+  EXPECT_EQ(result.result->outcome, mbf_msgs::action::GetPath::Result::SUCCESS);
+}
+
+TEST_F(PlannerActionFixture, tfError)
+{
+  // create a dummy path
+  std::vector<geometry_msgs::msg::PoseStamped> path(10);
+  // set the frame such that we fail at the tf
+  for (size_t ii = 0; ii != path.size(); ++ii)
+    path[ii].header.frame_id = "unknown";
+
+  // setup the expectation - we succeed here
+  EXPECT_CALL(*planner_, makePlan(_, _, _, _, _, _)).WillOnce(DoAll(SetArgReferee<3>(path), Return(0)));
+
+  // goal with frames, so we can pass tf-lookup
+  mbf_msgs::action::GetPath::Goal goal;
+  goal.use_start_pose = true;
+  goal.start_pose.header.frame_id = goal.target_pose.header.frame_id = global_frame_;
+
+  rclcpp_action::Client<mbf_msgs::action::GetPath>::WrappedResult result;
+  sendGoalAndWaitForResult(goal, result);
+  EXPECT_EQ(result.code, rclcpp_action::ResultCode::ABORTED);
+  EXPECT_EQ(result.result->outcome, mbf_msgs::action::GetPath::Result::TF_ERROR);
+}
+
+TEST_F(PlannerActionFixture, noRobotPose)
+{
+  // test case where we fail to get a valid robot pose.
+  // in this case we will receive a TF_ERROR from the server.
+
+  // make us use the robot pose
+  mbf_msgs::action::GetPath::Goal goal;
+  goal.use_start_pose = false;
+  goal.start_pose.header.frame_id = goal.target_pose.header.frame_id = global_frame_;
+
+  rclcpp_action::Client<mbf_msgs::action::GetPath>::WrappedResult result;
+  sendGoalAndWaitForResult(goal, result);
+  EXPECT_EQ(result.code, rclcpp_action::ResultCode::ABORTED);
+  EXPECT_EQ(result.result->outcome, mbf_msgs::action::GetPath::Result::TF_ERROR);
+}
+
+TEST_F(PlannerActionFixture, noPathFound)
+{
+  // test case where the planner fails.
+  // in this case we will receive NO_PLAN_FOUND from the server.
+  node_->set_parameter(rclcpp::Parameter("planner_max_retries", 0));
+
+  // valid goal
+  mbf_msgs::action::GetPath::Goal goal;
+  goal.use_start_pose = true;
+  goal.start_pose.header.frame_id = goal.target_pose.header.frame_id = global_frame_;
+
+  // set the expectation: the planner returns a failure
+  EXPECT_CALL(*planner_, makePlan(_, _, _, _, _, _)).WillOnce(Return(mbf_msgs::action::GetPath::Result::NO_PATH_FOUND));
+
+  rclcpp_action::Client<mbf_msgs::action::GetPath>::WrappedResult result;
+  sendGoalAndWaitForResult(goal, result);
+  EXPECT_EQ(result.code, rclcpp_action::ResultCode::ABORTED);
+  EXPECT_EQ(result.result->outcome, mbf_msgs::action::GetPath::Result::NO_PATH_FOUND);
+}
+
+ACTION(SleepAndFail)
+{
+  std::this_thread::sleep_for(std::chrono::milliseconds(10));
+  return 11;
+}
+
+TEST_F(PlannerActionFixture, patExceeded)
+{
+  // test case where the planner fails multiple times and we are out of patience
+
+  node_->set_parameter(rclcpp::Parameter("planner_max_retries", 5));
+  node_->set_parameter(rclcpp::Parameter("planner_patience", 0.001));
+
+  // valid goal
+  mbf_msgs::action::GetPath::Goal goal;
+  goal.use_start_pose = true;
+  goal.start_pose.header.frame_id = goal.target_pose.header.frame_id = global_frame_;
+
+  // set the expectation: the planner returns a failure
+  EXPECT_CALL(*planner_, makePlan(_, _, _, _, _, _)).WillRepeatedly(SleepAndFail());
+
+  rclcpp_action::Client<mbf_msgs::action::GetPath>::WrappedResult result;
+  sendGoalAndWaitForResult(goal, result);
+  EXPECT_EQ(result.code, rclcpp_action::ResultCode::ABORTED);
+  EXPECT_EQ(result.result->outcome, mbf_msgs::action::GetPath::Result::PAT_EXCEEDED);
+}
 
 int main(int argc, char** argv)
 {
