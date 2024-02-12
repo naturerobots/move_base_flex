c55ef586c695f79e3f7c3dd3d5a04afa70d25f47
Matthias Holoch <mholoch@gmail.com>
2024-02-12 15:46:50 +0100

    add robot simulator, use executor with two nodes

--- mbf_simple_nav/test/simple_nav_test.cpp
+++ mbf_simple_nav/test/simple_nav_test.cpp
@@ -1,9 +1,10 @@
-#include <optional>
-#include <gtest/gtest.h>
 #include <gmock/gmock.h>
+#include <gtest/gtest.h>
+#include <mbf_simple_nav/simple_navigation_server.h>
+#include <mbf_test_utility/robot_simulator.hpp>
+#include <optional>
 #include <rclcpp_action/client.hpp>
 #include <rclcpp/rclcpp.hpp>
-#include <mbf_simple_nav/simple_navigation_server.h>
 
 using namespace ::testing;
 
@@ -25,13 +26,25 @@ protected:
   void SetUp() override
   {
     rclcpp::init(0, nullptr);
+    executor_ptr_ = std::make_unique<rclcpp::executors::SingleThreadedExecutor>();
+    executor_ptr_->add_node(std::make_shared<mbf_test_utility::RobotSimulator>());
+    // node with simple server will be added later, in a method called from the individual test functions (to allow for setting parameter overrides)
+
     get_path_goal_.planner = "test_planner";
     get_path_goal_.use_start_pose = true;
     get_path_goal_.start_pose.header.frame_id = "odom";
+    // start pose starts at position [0,0,0]
     get_path_goal_.target_pose.header.frame_id = "odom";
     get_path_goal_.target_pose.pose.position.x = 10;
     get_path_goal_.target_pose.pose.position.y = -3;
+
     exe_path_goal_.controller = "test_controller";
+    exe_path_goal_.angle_tolerance = 0.1;
+    exe_path_goal_.dist_tolerance = 0.01;
+    exe_path_goal_.path.header.frame_id = "odom";
+    exe_path_goal_.path.poses.push_back(get_path_goal_.start_pose);
+    exe_path_goal_.path.poses.push_back(get_path_goal_.target_pose);
+
     recovery_goal_.behavior = "test_recovery";
   }
 
@@ -55,6 +68,7 @@ protected:
     action_client_recovery_ptr_ = rclcpp_action::create_client<mbf_msgs::action::Recovery>(
       node_ptr_,
       "simple_nav/recovery");
+    executor_ptr_->add_node(node_ptr_);
   }
 
   void TearDown() override
@@ -68,6 +82,7 @@ protected:
   std::shared_ptr<mbf_simple_nav::SimpleNavigationServer> nav_server_ptr_;
   std::shared_ptr<tf2_ros::Buffer> tf_buffer_ptr_;
   rclcpp::Node::SharedPtr node_ptr_;
+  std::unique_ptr<rclcpp::executors::SingleThreadedExecutor> executor_ptr_;
 
   // default parameterization for the tests
   const rclcpp::NodeOptions default_node_options_;
@@ -87,8 +102,8 @@ TEST_F(SimpleNavTest, rejectsGetPathGoalWhenNoPluginIsLoaded)
   initRosNode(rclcpp::NodeOptions()); // default node options without any parameter -> no plugins configured
   const auto goal_handle = action_client_get_path_ptr_->async_send_goal(get_path_goal_);
   ASSERT_EQ(
-    rclcpp::spin_until_future_complete(
-      node_ptr_, goal_handle, std::chrono::milliseconds(
+    executor_ptr_->spin_until_future_complete(
+      goal_handle, std::chrono::milliseconds(
         100)), rclcpp::FutureReturnCode::SUCCESS);
   EXPECT_THAT(goal_handle.get(), IsNull()); // goal rejection is expressed by returning a nullptr (by rclcpp_action client)
 }
@@ -98,8 +113,8 @@ TEST_F(SimpleNavTest, rejectsExePathGoalWhenNoPluginIsLoaded)
   initRosNode(rclcpp::NodeOptions());
   const auto goal_handle = action_client_exe_path_ptr_->async_send_goal(exe_path_goal_);
   ASSERT_EQ(
-    rclcpp::spin_until_future_complete(
-      node_ptr_, goal_handle, std::chrono::milliseconds(
+    executor_ptr_->spin_until_future_complete(
+      goal_handle, std::chrono::milliseconds(
         100)), rclcpp::FutureReturnCode::SUCCESS);
   EXPECT_THAT(goal_handle.get(), IsNull());
 }
@@ -109,8 +124,8 @@ TEST_F(SimpleNavTest, rejectsRecoveryGoalWhenNoPluginIsLoaded)
   initRosNode(rclcpp::NodeOptions());
   const auto goal_handle = action_client_recovery_ptr_->async_send_goal(recovery_goal_);
   ASSERT_EQ(
-    rclcpp::spin_until_future_complete(
-      node_ptr_, goal_handle, std::chrono::milliseconds(
+    executor_ptr_->spin_until_future_complete(
+      goal_handle, std::chrono::milliseconds(
         100)), rclcpp::FutureReturnCode::SUCCESS);
   EXPECT_THAT(goal_handle.get(), IsNull());
 }
@@ -123,8 +138,8 @@ TEST_F(SimpleNavTest, rejectsRecoveryGoalWhenNoPluginIsLoaded)
 //   planner_goal.planner = "test_planner";
 //   const auto goal_handle = action_client_get_path_ptr_->async_send_goal(planner_goal);
 //   ASSERT_EQ(
-//     rclcpp::spin_until_future_complete(
-//       node_ptr_, goal_handle,
+//     executor_ptr_->spin_until_future_complet(
+//       goal_handle,
 //       std::chrono::milliseconds(100)), rclcpp::FutureReturnCode::SUCCESS);
 //   EXPECT_THAT(goal_handle.get(), NotNull()); // goal was not rejected
 // }
@@ -135,11 +150,13 @@ TEST_F(SimpleNavTest, getPathReturnsPlan)
   initRosNode(default_node_options_);
   const auto goal_handle = action_client_get_path_ptr_->async_send_goal(get_path_goal_);
   ASSERT_EQ(
-    rclcpp::spin_until_future_complete(
-      node_ptr_, goal_handle,
-      std::chrono::milliseconds(100)), rclcpp::FutureReturnCode::SUCCESS);
+    executor_ptr_->spin_until_future_complete(
+      goal_handle, std::chrono::milliseconds(100)), rclcpp::FutureReturnCode::SUCCESS);
   const auto future_result = action_client_get_path_ptr_->async_get_result(goal_handle.get());
-  ASSERT_EQ(rclcpp::spin_until_future_complete(node_ptr_, future_result, std::chrono::seconds(1)), rclcpp::FutureReturnCode::SUCCESS);
+  ASSERT_EQ(
+    executor_ptr_->spin_until_future_complete(
+      future_result, std::chrono::seconds(
+        1)), rclcpp::FutureReturnCode::SUCCESS);
   // action is finished, result is available
   const mbf_msgs::action::GetPath::Result::SharedPtr result_ptr = future_result.get().result;
   EXPECT_EQ(result_ptr->outcome, mbf_msgs::action::GetPath::Result::SUCCESS);
