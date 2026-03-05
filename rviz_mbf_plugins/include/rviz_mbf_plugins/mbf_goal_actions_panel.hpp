b28883765fd69b8ce1ad7192df00305ddd6b6991
Alexander Mock <amock@uos.de>
2026-03-05 17:48:19 +0100

    changed destructor

--- rviz_mbf_plugins/include/rviz_mbf_plugins/mbf_goal_actions_panel.hpp
+++ rviz_mbf_plugins/include/rviz_mbf_plugins/mbf_goal_actions_panel.hpp
@@ -62,8 +62,7 @@ class MbfGoalActionsPanel : public rviz_common::Panel
 
 public:
   explicit MbfGoalActionsPanel(QWidget * parent = nullptr);
-  // ~MbfGoalActionsPanel() noexcept override = default;
-  virtual ~MbfGoalActionsPanel() {}
+  ~MbfGoalActionsPanel() noexcept override = default;
 
   void onInitialize() override;
   void save(rviz_common::Config config) const override;
